483fa418081ed25fd3a28a96876356115cd003f9
tehnotcpu <tehcpu@MacBook-Pro-Cpu.local>
2016-05-22 21:02:21 +0600

    AFK list fix

--- cmds/alert.py
+++ cmds/alert.py
@@ -6,12 +6,13 @@ class Alert(object):
     @checkrank(2)
     def _cmd_alert(self, cirno, username, args):
         afk = [i + ' ' for i in cirno.afklist if cirno.afklist[i]]
+        result = ''.join(afk)
         if not afk:
             cirno.sendmsg(username + ': Все на месте!')
         elif args:
-            cirno.sendmsg(args + ' ' + afk)
+            cirno.sendmsg(args + ' ' + result)
         else:
-            cirno.sendmsg(afk)
+            cirno.sendmsg(result)
 
 
 def setup():

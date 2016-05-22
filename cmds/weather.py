21e59a063edf30180a38077668477777a2065435
tehnotcpu <tehcpu@MacBook-Pro-Cpu.local>
2016-05-22 20:31:40 +0600

    Add forecast option for weather command

--- cmds/weather.py
+++ cmds/weather.py
@@ -22,14 +22,41 @@ class Weather(object):
         else:
             return 'Неверно указан город'
 
+    def forecast(self, city):
+        url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' \
+              + city + "&mode=json&lang=ru&units=metric&cnt=3&APPID=" + KEY
+        if city.isalpha():
+            try:
+                data = requests.get(url).json()['list']
+                temp = [i['temp']['day'] for i in data]
+                desc = [x['weather'][0]['description'] for x in data]
+                tomorrow = desc[1].capitalize() + ' ' + str(round(temp[1]))
+                aftertmrw = desc[2].capitalize() + ' ' + str(round(temp[2]))
+            except Exception:
+                return
+            return tomorrow, aftertmrw
+        else:
+            return 'Неверно указан город'
+
     def _cmd_weather(self, cirno, username, args):
-        result = self.weatherdata(args)
+        data = args.split()
         if not args:
             cirno.sendmsg(username + ': Укажите город!')
             return
+        city = data[0]
+        forecast = self.forecast(city)
+        today = self.weatherdata(city)
+        tomorrow = 'Погода в городе ' + city + \
+                   ' на завтра: ' + forecast[0] + '°C'
+        aftertomorrow = 'Погода в городе ' + city + \
+                        ' на послезавтра: ' + forecast[1] + '°C'
 
-        if result:
-            cirno.sendmsg(username + ': ' + result)
+        if today and len(data) < 2:
+            cirno.sendmsg(username + ': ' + today)
+        elif forecast and data[1] == 'завтра':
+            cirno.sendmsg(username + ': ' + tomorrow)
+        elif forecast and data[1] == 'послезавтра':
+            cirno.sendmsg(username + ': ' + aftertomorrow)
 
 
 def setup():

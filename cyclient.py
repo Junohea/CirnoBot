8e27fd9e79e4c811492ba2388e0e8671bf1d1a3f
tehnotcpu <cg@2ch.hk>
2016-06-07 21:36:32 +0600

    Gets socket port; config description

--- cyclient.py
+++ cyclient.py
@@ -1,19 +1,26 @@
 from socketIO_client import SocketIO
 from lib.cirno import Cirno
 from config import config
+import requests
 
 
 class Connections(object):
     def __init__(self):
         self.host = config['Server']['domain']
-        self.port = int(config['Server']['port'])
         self.channel = config['Server']['channel']
         self.channelpw = config['Server']['channelpass']
         self.name = config['Server']['login']
         self.password = config['Server']['password']
 
+    def getsocketport(self):
+        url = 'http://%s/socketconfig/%s.json' % (self.host, self.channel)
+        req = requests.get(url).json()['servers']
+        serv = [i['url'] for i in req if i['secure'] is False][0]
+        port = int(serv[serv.rindex(':') + 1:])
+        return port
+
     def cirnoconnect(self):
-        with SocketIO(self.host, self.port, Cirno) as socketIO:
+        with SocketIO(self.host, self.getsocketport(), Cirno) as socketIO:
             socketIO.emit('initChannelCallbacks')
             socketIO.emit('joinChannel', {'name': self.channel,
                                           'pw': self.channelpw})

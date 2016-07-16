4c918b961e3478959457f15d15d44503aaef7913
tehnotcpu <cg@2ch.hk>
2016-07-16 20:44:24 +0600

    Fixes, bump version

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='CirnoBot',
-    version='1.1.0',
-    packages=['lib', 'cmds'],
+    version='1.2.0',
     url='http://tehtube.tv/',
-    license='None',
+    license='MIT',
     author='tehcpu',
     author_email='admin@tehtube.tv',
-    description='', requires=['socketIO_client', 'requests',
-                              'oauth2client', 'bs4']
+    description='A Cytube bot',
+    install_requires=['socketIO_client', 'requests', 'bs4']
 )
